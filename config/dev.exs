748157aad07b6f830fe672ad1e845387498ba8ca
Patrice Gauthier <patgauth@gmail.com>
2023-03-01 23:17:59 -0500

    install tailwind

--- config/dev.exs
+++ config/dev.exs
@@ -26,7 +26,8 @@ config :wordling, WordlingWeb.Endpoint,
   secret_key_base: "M0eBBMTUXgOPv5aSDGmI0/sidJz5bi9XnWe2HNnPg0r11yE1ZtG18wfIvNJ7Sysz",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
