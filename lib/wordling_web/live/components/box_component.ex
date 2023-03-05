3b1515309bc25a86fbe1b9818624b041772e626a
Patrice Gauthier <patgauth@gmail.com>
2023-03-05 16:29:19 -0500

    add grid

---
+++ lib/wordling_web/live/components/box_component.ex
@@ -0,0 +1,13 @@
+defmodule WordlingWeb.Components.BoxComponent do
+  use WordlingWeb, :live_component
+
+  def render(assigns) do
+    ~L"""
+      <div class="table mx-1">
+        <div class="border-2 h-12 w-12 text-center table-cell align-middle">
+          <%= @letter %>
+        </div>
+      </div>
+    """
+  end
+end
