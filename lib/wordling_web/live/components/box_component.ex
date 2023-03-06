d22fa0ecad74028037c12f0d9a4a82527abf5a58
Patrice Gauthier <patgauth@gmail.com>
2023-03-05 21:09:54 -0500

    show as uppercase

--- lib/wordling_web/live/components/box_component.ex
+++ lib/wordling_web/live/components/box_component.ex
@@ -4,7 +4,7 @@ defmodule WordlingWeb.Components.BoxComponent do
   def render(assigns) do
     ~L"""
       <div class="table mx-1">
-        <div class="border-2 h-12 w-12 text-center table-cell align-middle">
+        <div class="border-2 h-12 w-12 text-center table-cell align-middle uppercase">
           <%= @letter %>
         </div>
       </div>
