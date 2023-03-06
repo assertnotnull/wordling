0c40e797956dc50367f1e7050d8b8d7233370b20
Patrice Gauthier <patgauth@gmail.com>
2023-03-05 21:10:27 -0500

    receive letters

--- lib/wordling_web/live/game_live.ex
+++ lib/wordling_web/live/game_live.ex
@@ -9,12 +9,42 @@ defmodule WordlingWeb.GameLive do
           ["a", "s", "d", "f", "g", "h", "j", "k", "l"],
           ["z", "x", "c", "v", "b", "n", "m"]
         ],
-        typed:
-          Enum.into(1..5, [], fn _x ->
-            Enum.into(1..5, [], fn _x -> "" end)
-          end)
+        typed: Enum.into(1..5, %{}, fn x -> {x, Enum.into(1..5, [], fn _x -> "" end)} end)
       )
 
     {:ok, socket}
+  end
+
+  def handle_event("send-letter", %{"key" => letter}, socket) do
+    is_letter_valid =
+      Enum.to_list(97..122)
+      |> to_string()
+      |> String.contains?(letter)
+
+    if(is_letter_valid || letter == "Backspace" || letter == "Enter") do
+      %{1 => first} = typed = socket.assigns.typed
+      updated = Map.put(typed, 1, handle_letter(letter, first))
+      socket = assign(socket, typed: updated)
+      {:noreply, socket}
+    else
+      {:noreply, socket}
+    end
+  end
+
+  def handle_letter("Backspace", row) do
+    case Enum.any?(row, fn x -> x == "" end) do
+      false ->
+        List.replace_at(row, 4, "")
+
+      true ->
+        List.replace_at(row, Enum.find_index(row, fn x -> x == "" end) - 1, "")
+    end
+  end
+
+  def handle_letter(letter, row) do
+    case Enum.any?(row, fn x -> x == "" end) do
+      false -> row
+      true -> List.replace_at(row, Enum.find_index(row, fn x -> x == "" end), letter)
+    end
   end
 end
